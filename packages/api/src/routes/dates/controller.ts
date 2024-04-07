18b5e934625fda5846ac3c194f23b2c11ef5e18e
ryanostrom <rpostrom@gmail.com>
2024-04-07 17:03:13 +0000

    more

--- packages/api/src/routes/dates/controller.ts
+++ packages/api/src/routes/dates/controller.ts
@@ -1,24 +1,43 @@
 import { NextFunction, Request, Response } from 'express'
+
 import { logger } from '@api/utils/logger'
 import HttpException from '@api/utils/HttpException'
-import getByGregorian from './methods/getByGregorian'
+import { findAllByGregorian } from './methods/gregorian'
+import {
+  findAllByHebrew,
+  isValidHebrewDateFormat,
+  parseHebrewDate
+} from './methods/hebrew'
 
 class DatesController {
   public getDates = async (req: Request, res: Response, next: NextFunction) => {
     try {
+      const type = typeof req.query.type === 'string' ? req.query.type : ''
       const start = typeof req.query.start === 'string' ? req.query.start : ''
       const end = typeof req.query.end === 'string' ? req.query.end : ''
+      let response
 
-      const dtStart = new Date(start)
-      const dtEnd = new Date(end)
+      if (type !== 'hebrew') {
+        const dtStart = new Date(start)
+        const dtEnd = new Date(end)
 
-      // Validate the date objects
-      if (isNaN(dtStart.getTime()) || isNaN(dtEnd.getTime())) {
-        next(new HttpException(400, 'Invalid start or end date'))
-        return
+        if (isNaN(dtStart.getTime()) || isNaN(dtEnd.getTime())) {
+          next(new HttpException(400, 'Invalid start or end date'))
+          return
+        }
+
+        response = await findAllByGregorian(dtStart, dtEnd)
+      } else {
+        if (!isValidHebrewDateFormat(start) || !isValidHebrewDateFormat(end)) {
+          next(new HttpException(400, 'Invalid start or end Hebrew date'))
+          return
+        }
+
+        response = await findAllByHebrew(
+          parseHebrewDate(start),
+          parseHebrewDate(end)
+        )
       }
-
-      const response = await getByGregorian(dtStart, dtEnd)
 
       if (!response || !response.length) {
         next(new HttpException(404, 'Dates not found'))
