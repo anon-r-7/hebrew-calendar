f06f8f1eed496657baaebe1b029364b5ab392de1
ryanostrom <rpostrom@gmail.com>
2024-04-18 18:29:03 +0000

    update kub

--- kubernetes/apply.sh
+++ kubernetes/apply.sh
@@ -7,7 +7,7 @@ kubectl --kubeconfig=k8s-hebrew-feasts-kubeconfig.yaml get pods --namespace ingr
 kubectl --kubeconfig=k8s-hebrew-feasts-kubeconfig.yaml get pods --namespace cert-manager
 
 # Investigate nginx controller
-kubectl --kubeconfig=k8s-hebrew-feasts-kubeconfig.yaml describe pod ingress-nginx-controller-7dcdbcff84-9zb2r --namespace ingress-nginx
+kubectl --kubeconfig=k8s-hebrew-feasts-kubeconfig.yaml describe pod ingress-nginx-controller-7dcdbcff84-gqcp9 --namespace ingress-nginx
 
 # Install cert manager cluster
 kubectl --kubeconfig=k8s-hebrew-feasts-kubeconfig.yaml apply -f config/letsencrypt-clusterissuer.yaml
@@ -36,3 +36,38 @@ kubectl --kubeconfig=k8s-hebrew-feasts-kubeconfig.yaml rollout restart deploymen
 
 # Update resources triggering rebuild
 kubectl --kubeconfig=k8s-hebrew-feasts-kubeconfig.yaml apply -f config
+
+# Get IP Addresses of load balancers
+kubectl --kubeconfig=k8s-hebrew-feasts-kubeconfig.yaml get svc
+
+# Cp postgres dump for docker locally
+docker cp 849df1474008:/tmp/hebrew_feasts_dump.sql ./dump.sql
+
+# SSH into pod
+kubectl --kubeconfig=k8s-hebrew-feasts-kubeconfig.yaml exec -it db-64cdfb6b58-j744m -- /bin/bash
+
+# Cp file to db
+kubectl --kubeconfig=k8s-hebrew-feasts-kubeconfig.yaml cp dump.sql db-64cdfb6b58-j744m  :/tmp/dump_file.sql
+
+# Import SQL 
+psql -U postgres -d hebrew_calendar -f /tmp/dump_file.sql
+
+# Verify sql import
+psql -U postgres -d hebrew_calendar -c "SELECT * FROM hebrew_events LIMIT 5;"
+
+
+# Stop deployments (scale to 0)
+kubectl --kubeconfig=k8s-hebrew-feasts-kubeconfig.yaml scale deployment/ui --replicas=0
+kubectl --kubeconfig=k8s-hebrew-feasts-kubeconfig.yaml scale deployment/api --replicas=0
+kubectl --kubeconfig=k8s-hebrew-feasts-kubeconfig.yaml scale deployment/db --replicas=0
+
+# Debugging once ssh'd into a node
+sudo journalctl -u kubelet
+dmesg | grep -i error
+sudo systemctl restart kubelet
+
+# Drain a node
+kubectl --kubeconfig=k8s-hebrew-feasts-kubeconfig.yaml drain hebrewfeast-pool-jgeg7 --ignore-daemonsets --delete-local-data
+kubectl --kubeconfig=k8s-hebrew-feasts-kubeconfig.yaml drain hebrewfeast-pool-jgeg7 --ignore-daemonsets --delete-local-data --force
+
+
