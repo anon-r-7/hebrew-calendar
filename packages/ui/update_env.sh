b25f3d4a8851b933ed0a07359b336bc3275a4144
ryanostrom <rpostrom@gmail.com>
2024-09-23 14:20:24 +0000

    update

--- packages/ui/update_env.sh
+++ packages/ui/update_env.sh
@@ -5,7 +5,7 @@ FILE_PATH="./build/index.html"
 
 # The content to replace and the new content with newline escape sequences
 SEARCH_STRING='<script id="env-vars"></script>'
-REPLACEMENT_STRING='<script id="env-vars">\n      var GLOBAL_UI_API_URL="http://api.hebrewfeasts.com";\n    </script>'
+REPLACEMENT_STRING='<script id="env-vars">\n      var GLOBAL_UI_API_URL="https://api.hebrewfeasts.com";\n    </script>'
 
 # Check if the file exists
 if [[ -f "$FILE_PATH" ]]; then
