fd22976c779712905c30ef2b60489bf324b52e19
Ryan Ostrom <rpostrom@gmail.com>
2026-09-11 11:53:59 -0500

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -28,72 +28,71 @@ should_deploy() {
 echo "Deploying: ${TARGETS:-all (api, ui, admin)}"
 
 # Ensure script is running from the root of the project
-ROOT_DIR="/var/www/hebrew-calendar"
+#ROOT_DIR="/var/www/hebrew-calendar"
+ROOT_DIR="$pwd"
+echo $pwd
 cd "$ROOT_DIR" || { echo "Error: Could not change to project root directory. Exiting..."; exit 1; }
 
 # Pull the latest from main before building
 echo "Pulling latest from main..."
-git checkout main
-check_status "Checking out main"
 git pull origin main
-check_status "Pulling latest from main"
 
 # Deploy API
 if should_deploy "api"; then
-  cd "$ROOT_DIR/packages/api" || { echo "Error: Could not change to API directory. Exiting..."; exit 1; }
+  # cd "$ROOT_DIR/packages/api" || { echo "Error: Could not change to API directory. Exiting..."; exit 1; }
 
-  echo "Stopping API..."
-  pm2 stop hebrew-calendar-api
-  check_status "Stopping API"
+  # echo "Stopping API..."
+  # pm2 stop hebrew-calendar-api
+  # check_status "Stopping API"
 
-  echo "Building API..."
-  yarn build
-  check_status "Building API"
+  # echo "Building API..."
+  # yarn build
+  # check_status "Building API"
 
-  echo "Starting API..."
-  pm2 start pm2.config.js --only hebrew-calendar-api
+  # echo "Starting API..."
+  # pm2 start pm2.config.js --only hebrew-calendar-api
   check_status "Starting API"
 fi
 
 # Deploy UI
 if should_deploy "ui"; then
-  cd "$ROOT_DIR/packages/ui" || { echo "Error: Could not change to UI directory. Exiting..."; exit 1; }
+  # cd "$ROOT_DIR/packages/ui" || { echo "Error: Could not change to UI directory. Exiting..."; exit 1; }
 
-  echo "Stopping UI..."
-  pm2 stop hebrew-calendar-ui
-  check_status "Stopping UI"
+  # echo "Stopping UI..."
+  # pm2 stop hebrew-calendar-ui
+  # check_status "Stopping UI"
 
-  echo "Building UI..."
-  yarn build
-  check_status "Building UI"
+  # echo "Building UI..."
+  # yarn build
+  # check_status "Building UI"
 
-  echo "Updating UI environment variables in index.html..."
-  ./update_env.sh
-  check_status "Updating UI environment variables"
+  # echo "Updating UI environment variables in index.html..."
+  # ./update_env.sh
+  # check_status "Updating UI environment variables"
 
-  echo "Starting UI..."
-  pm2 start pm2.config.js --only hebrew-calendar-ui
+  # echo "Starting UI..."
+  # pm2 start pm2.config.js --only hebrew-calendar-ui
   check_status "Starting UI"
 fi
 
 # Deploy admin
 if should_deploy "admin"; then
-  cd "$ROOT_DIR/packages/admin" || { echo "Error: Could not change to Admin directory. Exiting..."; exit 1; }
+  # cd "$ROOT_DIR/packages/admin" || { echo "Error: Could not change to Admin directory. Exiting..."; exit 1; }
 
-  echo "Stopping Admin..."
-  pm2 stop hebrew-calendar-admin
-  check_status "Stopping Admin"
+  # echo "Stopping Admin..."
+  # pm2 stop hebrew-calendar-admin
+  # check_status "Stopping Admin"
 
-  echo "Building Admin..."
-  yarn build
-  check_status "Building Admin"
+  # echo "Building Admin..."
+  # yarn build
+  # check_status "Building Admin"
 
-  echo "Updating Admin environment variables in index.html..."
-  ./update_env.sh
-  check_status "Updating Admin environment variables"
+  # echo "Updating Admin environment variables in index.html..."
+  # ./update_env.sh
+  # check_status "Updating Admin environment variables"
 
-  echo "Starting Admin..."
-  pm2 start pm2.config.js --only hebrew-calendar-admin
+  # echo "Starting Admin..."
+  # pm2 start pm2.config.js --only hebrew-calendar-admin
   check_status "Starting Admin"
 fi
 
